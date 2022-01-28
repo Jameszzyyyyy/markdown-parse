c181f2e36080087f407a6268d00ab4be515b1d16
Jameszzyyyyy <ziz021@ucsd.edu>
2022-01-27 19:29:14 -0800

    original

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -12,19 +12,11 @@ public class MarkdownParse {
         int currentIndex = 0;
         while(currentIndex < markdown.length()) {
             int nextOpenBracket = markdown.indexOf("[", currentIndex);
-            //if(nextOpenBracket < 0){break;}
             int nextCloseBracket = markdown.indexOf("]", nextOpenBracket);
             int openParen = markdown.indexOf("(", nextCloseBracket);
             int closeParen = markdown.indexOf(")", openParen);
             toReturn.add(markdown.substring(openParen + 1, closeParen));
-            if(nextOpenBracket == -1 || nextCloseBracket == -1 || openParen == -1 || closeParen == -1){
-                break;
-            }
             currentIndex = closeParen + 1;
-            if(nextOpenBracket == 0){
-                toReturn.add(markdown.substring(openParen + 1, closeParen));
-                currentIndex+=1;
-            }
         }
         return toReturn;
     }
