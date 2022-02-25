95269c8208e6e25ffa2724db5b4cdc4c2a71d779
Jameszzyyyyy <ziz021@ucsd.edu>
2022-02-24 19:49:58 -0800

    Update MarkdownParseTest.java

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -57,7 +57,7 @@ public class MarkdownParseTest {
     @Test
     public void testSnippet3() throws IOException{
         String contents = Files.readString(Path.of("./snippet3.md"));
-        List<String> expect = List.of("https://www.twitter.com", "github.com", "https://cse.ucsd.edu/");
+        List<String> expect = List.of("https://ucsd-cse15l-w22.github.io/");
         assertEquals(expect, MarkdownParse.getLinks(contents));
     }
 }
